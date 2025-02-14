f8519c8fc2b054477146bc507c844ee41e025eb0
Daniel-Fernando-662 <dovando0@ucol.mx>
2025-02-14 10:22:05 -0600

    Add files via upload

---
+++ holamundo.c
@@ -0,0 +1,14 @@
+// es un lenguaje "tipado"
+#include <studio.n> //directiva del compilador
+//prototipo de una funcion 
+void holamundo();
+//studio = standard input/output
+//.h libreria de encabezado header 
+void main (){
+        holamundo();
+}
+
+//funciones 
+void holamundo(){
+    printf("hola mundo");
+}
---
+++ hombre.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+void nombre();
+void paterno();
+void materno();
+
+void main(){
+    nombre();
+    paterno();
+    materno();
+}
+
+void nombre(){
+    printf("daniel fernando");
+}
+
+void paterno(){
+    printf("Ovando");
+}
+
+void materno(){
+    printf("Avalos");
+}
